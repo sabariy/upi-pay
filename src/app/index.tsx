a9d4aa5553910eb30aa888421cff72c53eb6265b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-11-09 08:48:16 +0530

    removed data.json usage😑

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,18 @@
 'use client'
 import { useState, useEffect } from "react";
-import Image from "next/image"
-import data from "../data.json"
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import BlockFalse from "./blockFalse";
 import BlockTrue from "./blockTrue";
 import BlockElse from "./blockElse";
+
 export default function Body() {
     const searchParams = useSearchParams();
     const id = "9633605648@fam";
     const name = "Sabarinath S";
-    const money = searchParams.get("money");
-    data.UPI = id;
-    data.NAME = name;
-    if(isNaN(Number(money))){
-        data.MONEY = "0";
-    }else{
-        data.MONEY = money || "";
-    }
+    const money = searchParams.get("money") || "0";
+    const validMoney = isNaN(Number(money)) ? "0" : money;
+
     return (
         <>
             <script>
